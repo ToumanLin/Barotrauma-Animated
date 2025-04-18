4df1f18df10fcd04ddadc5dd28a2418ae302c3f7
ToumanLin <72175577+ToumanLin@users.noreply.github.com>
2025-04-17 17:31:03 -0700

    update

---
+++ 00_木萌Release/CopyToHere.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal
+
+:: Get the name of this script
+set "SELF=%~nx0"
+
+:: Clear all files except this .bat file
+echo Cleaning current directory (excluding this script: %SELF%)...
+for %%f in (*.*) do (
+    if /I not "%%f"=="%SELF%" del /q "%%f"
+)
+
+:: Delete all subfolders
+for /d %%d in (*) do rd /s /q "%%d"
+pause
+
+:: Set source path
+set "SOURCE=C:\Program Files (x86)\Steam\steamapps\common\Barotrauma\LocalMods\Barotrauma-Animated"
+
+echo Generating item list…
+python "%SOURCE%\00_GenerateItemList.py"
+pause
+
+:: Copy folders
+echo Copying folders...
+xcopy "%SOURCE%\About" "About" /e /i /y
+xcopy "%SOURCE%\Content" "Content" /e /i /y
+xcopy "%SOURCE%\Subs" "Subs" /e /i /y
+
+:: Copy file
+echo Copying filelist.xml...
+copy "%SOURCE%\filelist.xml" "filelist.xml" /y
+pause
+
+:: Run the Python modifier
+echo Modifying filelist.xml with Python…
+python "%SOURCE%\00_modify_filelist.py" "filelist.xml"
+
+echo Done.
+pause
